b9c3f606546efbc63418b37da00f28ba319471d6
William Zou <wyzou@gra-login1.graham.sharcnet>
2022-05-26 12:27:30 -0400

    Add cifar vgg configuration files

---
+++ cifar-vgg19/cifar_vgg_c1_u20_iid1_dir1.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#SBATCH --account=def-desterck
+#SBATCH	--nodes=1
+#SBATCH	--gres=gpu:v100:1
+#SBATCH --nodelist=gra1337 
+#SBATCH --mem=32G
+#SBATCH --time=40:00:00
+
+
+REPEATS=1
+MODEL=vgg
+IID=1
+BIDIRECTIONAL=1
+EPOCH=200
+OPTIMIZER=sparsetopk
+DATASET=cifar
+FRAC=1
+GPU=cuda:0
+USERS=20
+LOCAL_BS=100
+TOPK=0.001
+TOPKD=0.001
+MP=1
+V=0
+
+export SLURM_TMPDIR=/home/wyzou/Federated-Learning-PyTorch
+module load python/3.7
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install torch torchvision tqdm tensorboardX six --no-index
+
+for LR in 0.1 0.12 0.15; do
+	for idx in $(seq 1 $REPEATS); do
+		srun python $SLURM_TMPDIR/src/federated_main.py --model=$MODEL --local_ep=1 --local_bs=$LOCAL_BS --frac=$FRAC --num_users=$USERS --optimizer=$OPTIMIZER \
+		 	--bidirectional=$BIDIRECTIONAL --dataset=$DATASET --gpu=$GPU --lr=$LR --iid=$IID --epochs=$EPOCH --topk=$TOPK --topk_d=$TOPKD --number=$idx --validation=$V --measure_parameters=$MP
+	done
+done
---
+++ cifar-vgg19/cifar_vgg_sgd_c1_u1_iid1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#SBATCH --account=def-desterck
+#SBATCH	--nodes=1
+#SBATCH	--gres=gpu:1
+#SBATCH --mem=8G
+#SBATCH --time=1:00:00
+
+
+REPEATS=1
+MODEL=vgg
+IID=1
+BIDIRECTIONAL=0
+EPOCH=1
+OPTIMIZER=sgd
+DATASET=cifar
+FRAC=1
+GPU=cuda:0
+USERS=1
+LOCAL_BS=256
+TOPK=0.001
+TOPKD=0.001
+MP=0
+V=0
+
+export SLURM_TMPDIR=/home/wyzou/Federated-Learning-PyTorch
+module load python/3.7
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install torch torchvision tqdm tensorboardX six --no-index
+
+for LR in 0.1; do
+	for idx in $(seq 1 $REPEATS); do
+		srun python $SLURM_TMPDIR/src/federated_main.py --model=$MODEL --local_ep=1 --local_bs=$LOCAL_BS --frac=$FRAC --num_users=$USERS --optimizer=$OPTIMIZER \
+		 	--bidirectional=$BIDIRECTIONAL --dataset=$DATASET --gpu=$GPU --lr=$LR --iid=$IID --epochs=$EPOCH --topk=$TOPK --topk_d=$TOPKD --number=$idx --validation=$V --measure_parameters=$MP
+	done
+done
